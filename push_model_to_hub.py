e13f05e38dabc1737f4073e06f1903532a970a75
davebulaval <david.beauchemin.5@ulaval.ca>
2023-11-22 17:54:32 -0500

    add push of tokenizer

--- push_model_to_hub.py
+++ push_model_to_hub.py
@@ -1,8 +1,11 @@
 import os.path
 
-from transformers import AutoModelForSequenceClassification
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
 model_path = os.path.join("datastore", "V1")
 model = AutoModelForSequenceClassification.from_pretrained(model_path)
 
 model.push_to_hub("davebulaval/MeaningBERT")
+
+tokenizer = AutoTokenizer.from_pretrained(model_path)
+tokenizer.push_to_hub("davebulaval/MeaningBERT")
