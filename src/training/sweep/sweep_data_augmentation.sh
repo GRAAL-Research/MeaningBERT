fca8a03bd299305289d97f017597dbb7abef0697
davebulaval <david.beauchemin.5@ulaval.ca>
2024-03-03 18:02:49 -0500

    fix module call

--- src/training/sweep/sweep_data_augmentation.sh
+++
@@ -1,4 +0,0 @@
-python -m salience_analysis.few_shot_training --seed=42 --data_augmentation="true"
-python -m salience_analysis.few_shot_training --seed=43 --data_augmentation="true"
-python -m salience_analysis.few_shot_training --seed=44 --data_augmentation="true"
-python -m salience_analysis.few_shot_training --seed=45 --data_augmentation="true"
---
+++ src/training/sweep_data_augmentation.sh
@@ -0,0 +1,4 @@
+python few_shot_training --seed=42 --data_augmentation="true"
+python few_shot_training --seed=43 --data_augmentation="true"
+python few_shot_training --seed=44 --data_augmentation="true"
+python few_shot_training --seed=45 --data_augmentation="true"
